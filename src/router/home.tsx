fb17379c01cb6e9e73cc2122eb2cca18f3e8abcc
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-07 18:28:21 -0500

    feat(unit-testing): Added change in home component

--- src/router/home.tsx
+++ src/router/home.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
+import { ListItem, ListItemButton, ListItemText } from '@mui/material'
+
+import HomeIcon from '@mui/icons-material/Home';
 
 import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  const navigate = useNavigate(); 
+    const navigate = useNavigate(); 
 
-  const handleClickAbout = () => {
-    navigate('/about-us'); 
-  };
-  const handleClickContact = () => {
-    navigate('/contact'); 
-  };
+    const handleClickAbout = () => {
+        navigate('/about-us'); 
+    };
 
-  return (
+return (
     <div>
-      <h2>Page of Home</h2>
-        <nav>
-          <button onClick={handleClickAbout}>About Us</button>
-          <button onClick={handleClickContact}>Contact</button>
-        </nav>
+        <h2>Page of Home</h2>
+            <nav>
+            <ListItem disablePadding sx={{backgroundColor:'#EEEEEE', borderRadius:'12px'}} onClick={handleClickAbout}>
+                <ListItemButton>
+                    <HomeIcon/>
+                    <ListItemText primary= 'Inicio'/>
+                </ListItemButton>
+            </ListItem>
+            </nav>
     </div>
-  )
+)
 }
